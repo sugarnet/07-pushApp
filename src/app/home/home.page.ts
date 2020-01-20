50838e01c7619c14e1470e3232eb417865bf50e3
Diego Scifo <diegoscifo@gmail.com>
2020-01-20 07:06:43 -0300

    end app pushApp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
+import { PushService } from '../services/push.service';
+import { OSNotificationPayload } from '@ionic-native/onesignal/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  messages: OSNotificationPayload[] = [];
 
+  constructor( public pushService: PushService, private applicationRef: ApplicationRef ) {}
+
+  ngOnInit() {
+
+    this.pushService.messagesListener.subscribe(notification => {
+      this.messages.unshift(notification);
+      this.applicationRef.tick();
+    });
+  }
+
+  async ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+    this.messages = await this.pushService.getMessages();
+  }
+
+  async deleteMessages() {
+    await this.pushService.deleteMessages();
+    this.messages = [];
+  }
 }
